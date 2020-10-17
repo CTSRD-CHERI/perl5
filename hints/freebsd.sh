ae02964bb360586369f56ec9247858e7d8e55196
Jessica Clarke <jrtc27@jrtc27.com>
2020-10-17 21:58:20 +0100

    hints/freebsd.sh: Respect -Dsysroot

--- hints/freebsd.sh
+++ hints/freebsd.sh
@@ -124,24 +124,24 @@ case "$osvers" in
 	;;
 
 3*|4*|5*|6*)
-        objformat=`/usr/bin/objformat`
+        objformat=`$sysroot/usr/bin/objformat`
         if [ x$objformat = xaout ]; then
-            if [ -e /usr/lib/aout ]; then
-                libpth="/usr/lib/aout /usr/local/lib /usr/lib"
-                glibpth="/usr/lib/aout /usr/local/lib /usr/lib"
+            if [ -e $sysroot/usr/lib/aout ]; then
+                libpth="$sysroot/usr/lib/aout $sysroot/usr/local/lib $sysroot/usr/lib"
+                glibpth="$sysroot/usr/lib/aout $sysroot/usr/local/lib $sysroot/usr/lib"
             fi
             lddlflags='-Bshareable'
         else
-            libpth="/usr/lib /usr/local/lib"
-            glibpth="/usr/lib /usr/local/lib"
+            libpth="$sysroot/usr/lib $sysroot/usr/local/lib"
+            glibpth="$sysroot/usr/lib $sysroot/usr/local/lib"
             ldflags="-Wl,-E "
             lddlflags="-shared "
         fi
         cccdlflags='-DPIC -fPIC'
         ;;
 *)
-       libpth="/usr/lib /usr/local/lib"
-       glibpth="/usr/lib /usr/local/lib"
+       libpth="$sysroot/usr/lib $sysroot/usr/local/lib"
+       glibpth="$sysroot/usr/lib $sysroot/usr/local/lib"
        ldflags="-Wl,-E "
         lddlflags="-shared "
         cccdlflags='-DPIC -fPIC'
@@ -153,7 +153,7 @@ case "$osvers" in
 
 *)
 	ccflags="${ccflags} -DHAS_FPSETMASK -DHAS_FLOATINGPOINT_H"
-	if /usr/bin/file -L /usr/lib/libc.so | /usr/bin/grep -vq "not stripped" ; then
+	if /usr/bin/file -L $sysroot/usr/lib/libc.so | /usr/bin/grep -vq "not stripped" ; then
 	    usenm=false
 	fi
         ;;
