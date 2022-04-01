5d3c8b34540ad8e50d1431555fccf3310d7dee27
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2022-04-01 23:44:53 +0000

    Correctly fill mode with zeroes.
    Use the actual size of the mode array - PERL_MODE_MAX instead of a size
    of the mode pointer.

--- doio.c
+++ doio.c
@@ -363,7 +363,7 @@ S_openn_setup(pTHX_ GV *gv, char *mode, PerlIO **saveifp, PerlIO **saveofp,
     *savefd = -1;
     *savetype = IoTYPE_CLOSED;
 
-    Zero(mode,sizeof(mode),char);
+    Zero(mode, PERL_MODE_MAX, char);
     PL_forkprocess = 1;		/* assume true if no fork */
 
     /* If currently open - close before we re-open */
