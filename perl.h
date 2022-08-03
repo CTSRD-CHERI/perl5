f5b984a54d871d56adae74568d8092d91ce74ef0
Konrad Witaszczyk <konrad.witaszczyk@cl.cam.ac.uk>
2022-08-03 11:20:36 +0000

    Use {IV,UV}INT for bitwise operations.
    Instead of IV/UV, use {IV,UV}INT to perform bitwse operation on integer
    values stored with IV/UV.

--- perl.h
+++ perl.h
@@ -2810,9 +2810,9 @@ the highest precision one available is used.
 #define PERL_LONG_MIN ((long)LONG_MIN)
 
 #ifdef UV_IS_QUAD
-#    define PERL_UQUAD_MAX	(~(UV)0)
-#    define PERL_UQUAD_MIN	((UV)0)
-#    define PERL_QUAD_MAX 	((IV) (PERL_UQUAD_MAX >> 1))
+#    define PERL_UQUAD_MAX	(~(UVINT)0)
+#    define PERL_UQUAD_MIN	((UVINT)0)
+#    define PERL_QUAD_MAX 	((IVINT) (PERL_UQUAD_MAX >> 1))
 #    define PERL_QUAD_MIN 	(-PERL_QUAD_MAX - ((3 & -1) == 3))
 #endif
 
